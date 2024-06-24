6f3beba2465ad267c4676dd136b779032dac3f87
VietSlayerSera <renanytofc@gmail.com>
2024-06-24 16:00:47 -0300

    Update AnyfeelsHub.lua

--- AnyfeelsHub.lua
+++ AnyfeelsHub.lua
@@ -42,32 +42,40 @@ if game.PlaceId == 3237168 then
     spawn(GetAnyMelee)
 
     -- Função para imprimir dados
-    function DataPrinter()
+    function DataPrinter(outputBox)
         while _G.DataPrinterEnabled do
             local player = game.Players.LocalPlayer
             local userData = game.Workspace.UserData:FindFirstChild("User_" .. player.UserId)
-            local Data = userData.Data
-            print(player.UserId)
-            print("Name:", player.Name)
-            print("Beri:", Data.Cash.Value)
-            print("Bounty:", Data.Bounty.Value)
-            print("Compasses:", Data.CompassTokens.Value)
-            print("Gems:", Data.Gems.Value)
-            print("Kills:", Data.Kills.Value)
-            print("DF1:", Data.DevilFruit.Value)
-            print("DF2:", Data.DevilFruit2.Value)
-            print("StoredDF1:", Data.StoredDF1.Value)
-            print("StoredDF2:", Data.StoredDF2.Value)
-            print("StoredDF3:", Data.StoredDF3.Value)
-            print("StoredDF4:", Data.StoredDF4.Value)
-            print("StoredDF5:", Data.StoredDF5.Value)
-            print("StoredDF6:", Data.StoredDF6.Value)
-            print("StoredDF7:", Data.StoredDF7.Value)
-            print("StoredDF8:", Data.StoredDF8.Value)
-            print("StoredDF9:", Data.StoredDF9.Value)
-            print("StoredDF10:", Data.StoredDF10.Value)
-            print("StoredDF11:", Data.StoredDF11.Value)
-            print("StoredDF12:", Data.StoredDF12.Value)
+            if userData then
+                local Data = userData.Data
+                local output = {
+                    "UserId: " .. player.UserId,
+                    "Name: " .. player.Name,
+                    "Beri: " .. Data.Cash.Value,
+                    "Bounty: " .. Data.Bounty.Value,
+                    "Compasses: " .. Data.CompassTokens.Value,
+                    "Gems: " .. Data.Gems.Value,
+                    "Kills: " .. Data.Kills.Value,
+                    "DF1: " .. Data.DevilFruit.Value,
+                    "DF2: " .. Data.DevilFruit2.Value,
+                    "StoredDF1: " .. Data.StoredDF1.Value,
+                    "StoredDF2: " .. Data.StoredDF2.Value,
+                    "StoredDF3: " .. Data.StoredDF3.Value,
+                    "StoredDF4: " .. Data.StoredDF4.Value,
+                    "StoredDF5: " .. Data.StoredDF5.Value,
+                    "StoredDF6: " .. Data.StoredDF6.Value,
+                    "StoredDF7: " .. Data.StoredDF7.Value,
+                    "StoredDF8: " .. Data.StoredDF8.Value,
+                    "StoredDF9: " .. Data.StoredDF9.Value,
+                    "StoredDF10: " .. Data.StoredDF10.Value,
+                    "StoredDF11: " .. Data.StoredDF11.Value,
+                    "StoredDF12: " .. Data.StoredDF12.Value
+                }
+                outputBox:SetText(table.concat(output, "\n"))
+            else
+                warn("User data not found for player: " .. player.Name)
+                outputBox:SetText("User data not found for player: " .. player.Name)
+            end
             wait(5)  -- Intervalo de tempo entre impressões
         end
     end
@@ -103,14 +111,30 @@ if game.PlaceId == 3237168 then
         PremiumOnly = false
     })
 
-    -- Adiciona um toggle na aba Uteis
-    UteisTab:AddToggle({
-        Name = "Data Printer",
+    -- Cria a seção dentro da aba Uteis
+    local UteisSection = UteisTab:AddSection({
+        Name = "Data Printer"
+    })
+
+    -- Adiciona uma área de texto para o output
+    local outputBox = UteisSection:AddTextbox({
+        Name = "Output",
+        Text = "",
+        MultiLine = true,
+        ClearTextOnFocus = false,
+        TextEditable = false
+    })
+
+    -- Adiciona um toggle na seção Uteis
+    UteisSection:AddToggle({
+        Name = "Enable Data Printer",
         Default = false,
         Callback = function(Value)
             _G.DataPrinterEnabled = Value
             if _G.DataPrinterEnabled then
-                spawn(DataPrinter)
+                spawn(function() DataPrinter(outputBox) end)
+            else
+                outputBox:SetText("")  -- Limpa a área de texto quando desativado
             end
         end
     })
